5b1df1c67a2cc029a47c8d77a19e6329d76ed7f2
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-05-21 00:12:06 +0530

    Add Binary Serach Tree Insert and Find methods

--- ConsoleApp/BinarSearchTree.cs
+++ ConsoleApp/BinarSearchTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CustomLibrary;
 
 namespace ConsoleApp
 {
@@ -8,7 +9,50 @@ namespace ConsoleApp
         {
             try
             {
+                #region CustomBinarySearchTree
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Implementing the Binary Search Tree with basic Operations");
+                Console.ForegroundColor = ConsoleColor.White;
 
+                CustomBinarySearchTree customBinarySearchTree = new CustomBinarySearchTree();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the elements 10, 5, 15, 6, 1, 8, 12, 18, 17 to Binary Search Tree");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                customBinarySearchTree.Insert(10);
+                customBinarySearchTree.Insert(5);
+                customBinarySearchTree.Insert(15);
+                customBinarySearchTree.Insert(6);
+                customBinarySearchTree.Insert(1);
+                customBinarySearchTree.Insert(8);
+                customBinarySearchTree.Insert(12);
+                customBinarySearchTree.Insert(18);
+                customBinarySearchTree.Insert(6);
+                customBinarySearchTree.Insert(17);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Finding the element 15 in Binary Search Tree");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(customBinarySearchTree.Find(15));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Finding the element 12 in Binary Search Tree");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(customBinarySearchTree.Find(12));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Finding the element 20 in Binary Search Tree");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(customBinarySearchTree.Find(20));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Finding the element 6 in Binary Search Tree");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(customBinarySearchTree.Find(6));
+
+                Console.ReadKey();
+                #endregion
             }
             catch (Exception ex)
             {
--- CustomLibrary/CustomBinarySearchTree.cs
+++ CustomLibrary/CustomBinarySearchTree.cs
@@ -4,5 +4,92 @@ namespace CustomLibrary
 {
     public class CustomBinarySearchTree
     {
+        #region private fileds
+        private class Node
+        {
+            public int value;
+            public Node leftChild;
+            public Node rightChild;
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        private Node root;
+        #endregion
+
+        #region public methods
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+
+            if (IsEmpty())
+            {
+                root = node;
+            }
+            else
+            {
+                var current = root;
+
+                while (true)
+                {
+                    if (value < current.value)
+                    {
+                        if (current.leftChild == null)
+                        {
+                            current.leftChild = node;
+                            break;
+                        }
+                        current = current.leftChild;
+                    }
+                    else
+                    {
+                        if (current.rightChild == null)
+                        {
+                            current.rightChild = node;
+                            break;
+                        }
+                        current = current.rightChild;
+                    }
+                }
+            }
+        }
+
+        public bool Find(int value)
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Tree is empty");
+            }
+
+            var current = root;
+            while (current != null)
+            {
+                if (value < current.value)
+                {
+                    current = current.leftChild;
+                }
+                else if (value > current.value)
+                {
+                    current = current.rightChild;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
+
+        #region private methods
+        private bool IsEmpty()
+        {
+            return root == null;
+        }
+        #endregion
     }
 }
