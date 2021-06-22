dfd6a7a57583b17dd0f509fa3ea0e250864aa87c
Harsha <harshasanapu100@gmail.com>
2021-06-22 23:18:20 +0530

    Add AddNode, AddEdge, RemoveNode and RemoveEdge methods to graph class

--- ConsoleApp/Graph.cs
+++ ConsoleApp/Graph.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CustomLibrary;
 
 namespace ConsoleApp
 {
@@ -10,7 +9,40 @@ namespace ConsoleApp
         {
             try
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Implementing the Graph with basic operations");
+                Console.ForegroundColor = ConsoleColor.White;
 
+                CustomGraph customGraph = new CustomGraph();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the lables A, B, C to graph");
+                Console.WriteLine("Adding edges A-->B, A-->C, B-->C, B-->A");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                customGraph.AddNode("A");
+                customGraph.AddNode("B");
+                customGraph.AddNode("C");
+
+                customGraph.AddEdge("A", "B");
+                customGraph.AddEdge("A", "C");
+                customGraph.AddEdge("B", "C");
+                customGraph.AddEdge("B", "A");
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing nodes connected with edges");
+                Console.ForegroundColor = ConsoleColor.White;
+                customGraph.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Removing edge A-->B and B-->C");
+                Console.ForegroundColor = ConsoleColor.White;
+                customGraph.RemoveEdge("A", "B");
+                customGraph.RemoveEdge("B", "C");
+                customGraph.Print();
             }
             catch (Exception ex)
             {
--- CustomLibrary/CustomGraph.cs
+++ CustomLibrary/CustomGraph.cs
@@ -6,5 +6,104 @@ namespace CustomLibrary
 {
     public class CustomGraph
     {
+        #region private fields
+        private class Node
+        {
+            public string label;
+
+            public Node(String label)
+            {
+                this.label = label;
+            }
+
+        }
+
+        private Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+        private Dictionary<Node, List<Node>> adjacencyList = new Dictionary<Node, List<Node>>();
+        #endregion
+
+        #region public methods
+        public void AddNode(string label)
+        {
+            Node node = new Node(label);
+
+            if (!nodes.ContainsKey(label))
+            {
+                nodes.Add(label, node);
+                adjacencyList.Add(node, new List<Node>());
+            }
+        }
+
+        public void AddEdge(string from, string to)
+        {
+            Node fromNode = nodes.ContainsKey(from) ? nodes[from] : null;
+            if (fromNode == null)
+            {
+                throw new Exception("Node or vertex is not found");
+            }
+
+            Node toNode = nodes.ContainsKey(to) ? nodes[to] : null;
+            if (toNode == null)
+            {
+                throw new Exception("Node or vertex is not found");
+            }
+
+            adjacencyList[fromNode].Add(toNode);
+        }
+
+        public void RemoveNode(string label)
+        {
+            Node node = nodes.ContainsKey(label) ? nodes[label] : null;
+
+            if (node == null)
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<Node, List<Node>> kvp in adjacencyList)
+            {
+                adjacencyList[kvp.Key].Remove(node);
+            }
+
+            adjacencyList.Remove(node);
+            nodes.Remove(label);
+        }
+
+        public void RemoveEdge(string from, string to)
+        {
+            Node fromNode = nodes.ContainsKey(from) ? nodes[from] : null;
+            if (fromNode == null)
+            {
+                return;
+            }
+
+            Node toNode = nodes.ContainsKey(to) ? nodes[to] : null;
+            if (toNode == null)
+            {
+                return;
+            }
+
+            adjacencyList[fromNode].Remove(toNode);
+        }
+
+        public void Print()
+        {
+            foreach (KeyValuePair<Node, List<Node>> kvp in adjacencyList)
+            {
+                if (kvp.Value.Count > 0)
+                {
+                    string edges = "[ ";
+                    foreach (Node item in kvp.Value)
+                    {
+                        edges += item.label + " ";
+                    }
+
+                    edges += "]";
+
+                    Console.WriteLine(kvp.Key.label + " is connected to " + edges);
+                }
+            }
+        }
+        #endregion
     }
 }
